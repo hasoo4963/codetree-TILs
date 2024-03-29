b4ebf61cc35ab47609086e042eb720cc281d3a75
SWIMING <hasoo4963@gmail.com>
2024-03-29 16:08:12 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240329/데이터 교환/exchange-data.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 5;
+    int b = 6;
+    int c = 7;
+
+    int tempA = a, tempB = b, tempC = c;
+
+    b = tempA;
+    c = tempB;
+    a = tempC;
+
+    cout << a << "\n" << b << "\n" << c;
+
+    return 0;
+}
