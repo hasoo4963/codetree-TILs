22635d70bba1635fd782d2146a1f33262930da45
SWIMING <hasoo4963@gmail.com>
2024-04-04 14:30:45 +0900

    Title: 그 계절, 그 날, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240404/그 계절, 그 날/that-season-that-day.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+bool isLeapYear(int year){
+    if (year % 4 != 0){
+        return false;
+    }
+
+    if (year % 100 != 0){
+        return true;
+    }
+
+    if (year % 400 == 0){
+        return true;
+    }
+
+    return false;
+}
+
+bool isAvailable(int year,int month,int day){
+    if(isLeapYear(year)){
+        if(month == 2){
+            if(day <= 29){
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    }
+    else {
+        return true;
+    }
+}
+
+void whatSeason(int year, int month, int day){
+    if(month >= 3 && month <= 5){
+        cout << "Spring" << endl;
+    } else if (month >= 6 && month <= 8){
+        cout << "Summer" << endl;
+    } else if (month >= 9 && month <= 11){
+        cout << "Fall" << endl;
+    } else {
+        cout << "Winter" << endl;
+    }
+}
+
+void printSeason(int year, int month, int day){
+    if(isAvailable(year,month,day)){
+        whatSeason(year,month,day);
+    }
+    else {
+        cout << -1 << endl;
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int year, month, day;
+    cin >> year >> month >> day;
+
+    printSeason(year,month,day);
+
+    return 0;
+}
