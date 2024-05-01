f2eceee72260232d2797823ae3e249146c902683
SWIMING <hasoo4963@gmail.com>
2024-05-01 19:37:25 +0900

    Title: Top K 숫자 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240501/Top K 숫자 구하기/kth-number.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int N;
+    int k;
+    cin >> N >> k;
+
+    int arr[N];
+
+    for (int i = 0; i < N; i++){
+        cin >> arr[i];
+    }
+
+    sort(arr, arr+N);
+    
+    cout << arr[k-1] << "\n";
+
+    return 0;
+}
