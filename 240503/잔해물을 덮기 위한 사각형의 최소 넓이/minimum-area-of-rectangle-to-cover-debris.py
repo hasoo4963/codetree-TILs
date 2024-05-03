9702be56c348cc98550ccfa996bd9301e051d3ce
SWIMING <hasoo4963@gmail.com>
2024-05-03 21:55:43 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 211ms, Memory: 55MB, Status: Wrong Answer - CodeTree

--- 240503/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240503/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -1,7 +1,7 @@
-def draw(arr_2d, x1, y1, x2, y2):
+def draw(arr_2d, x1, y1, x2, y2, value):
     for i in range(x1, x2):
         for j in range(y1, y2):
-            arr_2d[i][j] += 1
+            arr_2d[i][j] = value
 
 def isComplete(arr_2d):
     for i in range(len(arr_2d)):
@@ -17,8 +17,8 @@ arr_2d = [
     for _ in range(2002)
 ]
 
-draw(arr_2d, first_x1, first_y1, first_x2, first_y2)
-draw(arr_2d, second_x1, second_y1, second_x2, second_y2)
+draw(arr_2d, first_x1, first_y1, first_x2, first_y2, 1)
+draw(arr_2d, second_x1, second_y1, second_x2, second_y2, 2)
 
 if isComplete(arr_2d):
     print(0)
