b24262fb521dd74ff2473c719ec7b91eaca76b5d
SWIMING <hasoo4963@gmail.com>
2024-05-07 21:28:36 +0900

    Title: 문자에 따른 명령 2, Time: 127ms, Memory: 27MB, Status: Passed - CodeTree

--- 240507/문자에 따른 명령 2/text-based-commands2.py
+++ 240507/문자에 따른 명령 2/text-based-commands2.py
@@ -12,7 +12,7 @@ for i in range(N):
     if direction[i] == "L":
         dir_num = (dir_num + 1) % 4
     elif direction[i] == "R":
-        dir_num = (dir_num - 1 + 3) % 4
+        dir_num = (dir_num - 1 + 4) % 4
     else:
         x = x + dxs[dir_num]
         y = y + dys[dir_num]
