551923a13c52f532797c0bb4f0158edafc9a4816
SWIMING <hasoo4963@gmail.com>
2024-05-09 21:34:55 +0900

    Title: 이상한 진수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240509/이상한 진수 2/awkward-digits-2.py
+++ 240509/이상한 진수 2/awkward-digits-2.py
@@ -1,12 +1,30 @@
+def isAll1(arr):
+    for i in range(len(arr)):
+        if arr[i] == "0":
+            return False
+    return True
+# 요소가 전부 1인지 검사하는 코드
+
 S = list(input())
 length = len(S)
 
 sum = 0
 
-for i in range(length):
-    if S[i] != "1":
-        S[i] = '1'
-        break
+if length == 1:
+    if S[i] == "1":
+        S[i] = "0"
+    else:
+        S[i] = "1"
+#length가 1인지 검사해 적절한 조치 취하기
+else:
+    if isAll1(S):
+        S[length - 1] = "0"
+    else:
+        for i in range(length):
+            if S[i] != "1":
+                S[i] = '1'
+                break
+#length가 0은 아니지만 모두 1인경우에 0을 한군데에는 달아줘야함
 
 for i in range(length):
     sum = sum + (2**(length - 1 - i))*int(S[i])
