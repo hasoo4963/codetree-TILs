51641e4aff792f038cb2d215e30d89c2365c8662
SWIMING <hasoo4963@gmail.com>
2024-05-09 20:34:54 +0900

    Title: 모이자, Time: 113ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240509/모이자/gather.py
@@ -0,0 +1,19 @@
+n = int(input())
+arr = list(map(int,input().split()))
+
+result = []
+
+min_val = 100000
+for i in range(n): # i번째 집으로 모이면,
+    sum = 0
+    for j in range(i+1, n):
+        diff = j - i
+        sum = sum + (diff * arr[j])
+
+    for k in range(i):
+        diff = i - k
+        sum = sum + (diff * arr[k])
+
+    result.append(sum)
+
+print(min(result))
